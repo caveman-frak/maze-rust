457e74f8e19a037d2cf55dd4dfa381189e5b4647
Caveman Frak <tim.pickard@bluegecko.co.uk>
2021-03-21 10:42:09 +0000

    added display methods

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,16 @@ fn main() {
     let grid = grid::Grid::grid(2, 3);
 
     println!("grid = {},{}", grid.rows(), grid.columns());
-    println!("grid = {:?}", grid);
+    println!("grid = {}", grid);
 
     let cell = grid.cell(1, 1);
 
-    println!("cell = {:?}", cell);
+    println!("cell = {:?} or {}", cell, cell);
     println!(
-        "N = {:?}, E = {}, S = {:?}, W = {:?}",
+        "neighbours -> N = {:?}, E = {}, S = {:?}, W = {:?}",
         grid::print_cell(grid.neighbour(cell, grid::Direction::North)),
         grid::print_cell(grid.neighbour(cell, grid::Direction::East)),
         grid::print_cell(grid.neighbour(cell, grid::Direction::South)),
         grid::print_cell(grid.neighbour(cell, grid::Direction::West))
     );
-
 }
